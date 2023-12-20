93f8fa8a7357390c174111371be462b8cbfe5790
Emre Za <emresabotic10@outlook.com>
2023-12-20 16:11:44 +0300

    Add project files.

---
+++ maaşprojesi/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace maaşprojesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTelefon = new DevExpress.XtraEditors.TextEdit();
+            this.txtID = new DevExpress.XtraEditors.TextEdit();
+            this.txtBonus = new DevExpress.XtraEditors.TextEdit();
+            this.txtMaas = new DevExpress.XtraEditors.TextEdit();
+            this.txtGaranti = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl8 = new DevExpress.XtraEditors.LabelControl();
+            this.txtAdres = new System.Windows.Forms.RichTextBox();
+            this.labelControl9 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit8 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl10 = new DevExpress.XtraEditors.LabelControl();
+            this.btnHesapla = new DevExpress.XtraEditors.SimpleButton();
+            this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTelefon.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtBonus.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMaas.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGaranti.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit8.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(111, 117);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTelefon.Properties.Appearance.Options.UseFont = true;
+            this.txtTelefon.Size = new System.Drawing.Size(338, 34);
+            this.txtTelefon.TabIndex = 1;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(111, 23);
+            this.txtID.Name = "txtID";
+            this.txtID.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtID.Properties.Appearance.Options.UseFont = true;
+            this.txtID.Size = new System.Drawing.Size(338, 34);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtBonus
+            // 
+            this.txtBonus.Location = new System.Drawing.Point(842, 506);
+            this.txtBonus.Name = "txtBonus";
+            this.txtBonus.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBonus.Properties.Appearance.Options.UseFont = true;
+            this.txtBonus.Size = new System.Drawing.Size(270, 34);
+            this.txtBonus.TabIndex = 4;
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Location = new System.Drawing.Point(1179, 506);
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMaas.Properties.Appearance.Options.UseFont = true;
+            this.txtMaas.Size = new System.Drawing.Size(270, 34);
+            this.txtMaas.TabIndex = 5;
+            // 
+            // txtGaranti
+            // 
+            this.txtGaranti.Location = new System.Drawing.Point(499, 506);
+            this.txtGaranti.Name = "txtGaranti";
+            this.txtGaranti.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGaranti.Properties.Appearance.Options.UseFont = true;
+            this.txtGaranti.Size = new System.Drawing.Size(270, 34);
+            this.txtGaranti.TabIndex = 6;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(13, 34);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(27, 18);
+            this.labelControl1.TabIndex = 7;
+            this.labelControl1.Text = "ID :";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(13, 80);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(78, 18);
+            this.labelControl2.TabIndex = 8;
+            this.labelControl2.Text = "Ad Soyad :";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Location = new System.Drawing.Point(13, 120);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(65, 18);
+            this.labelControl4.TabIndex = 10;
+            this.labelControl4.Text = "Telefon :";
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl5.Appearance.Options.UseFont = true;
+            this.labelControl5.Location = new System.Drawing.Point(574, 453);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(117, 21);
+            this.labelControl5.TabIndex = 11;
+            this.labelControl5.Text = "Garanti Para :";
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl6.Appearance.Options.UseFont = true;
+            this.labelControl6.Location = new System.Drawing.Point(917, 453);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(119, 21);
+            this.labelControl6.TabIndex = 12;
+            this.labelControl6.Text = "Bonus Gelen :";
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl7.Appearance.Options.UseFont = true;
+            this.labelControl7.Location = new System.Drawing.Point(1276, 453);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(93, 21);
+            this.labelControl7.TabIndex = 13;
+            this.labelControl7.Text = "Son Maaş :";
+            // 
+            // labelControl8
+            // 
+            this.labelControl8.Appearance.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl8.Appearance.Options.UseFont = true;
+            this.labelControl8.Location = new System.Drawing.Point(13, 232);
+            this.labelControl8.Name = "labelControl8";
+            this.labelControl8.Size = new System.Drawing.Size(52, 18);
+            this.labelControl8.TabIndex = 15;
+            this.labelControl8.Text = "Adres :";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdres.Location = new System.Drawing.Point(111, 221);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(338, 325);
+            this.txtAdres.TabIndex = 16;
+            this.txtAdres.Text = "";
+            // 
+            // labelControl9
+            // 
+            this.labelControl9.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl9.Appearance.Options.UseFont = true;
+            this.labelControl9.Location = new System.Drawing.Point(596, 305);
+            this.labelControl9.Name = "labelControl9";
+            this.labelControl9.Size = new System.Drawing.Size(200, 21);
+            this.labelControl9.TabIndex = 18;
+            this.labelControl9.Text = "Aşağıda Yapılan İşlem :";
+            // 
+            // textEdit8
+            // 
+            this.textEdit8.EditValue = "Bonus gelen paranın %10\'u ve garanti paranın toplamı çalışanın son maaşıdır..";
+            this.textEdit8.Location = new System.Drawing.Point(852, 302);
+            this.textEdit8.Name = "textEdit8";
+            this.textEdit8.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textEdit8.Properties.Appearance.Options.UseFont = true;
+            this.textEdit8.Size = new System.Drawing.Size(597, 28);
+            this.textEdit8.TabIndex = 17;
+            // 
+            // labelControl10
+            // 
+            this.labelControl10.Appearance.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl10.Appearance.Options.UseFont = true;
+            this.labelControl10.Location = new System.Drawing.Point(683, 12);
+            this.labelControl10.Name = "labelControl10";
+            this.labelControl10.Size = new System.Drawing.Size(625, 40);
+            this.labelControl10.TabIndex = 19;
+            this.labelControl10.Text = "-Çalışan Son Maaş Hesaplama Formu-";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Appearance.Options.UseFont = true;
+            this.btnHesapla.Location = new System.Drawing.Point(1071, 140);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(142, 50);
+            this.btnHesapla.TabIndex = 20;
+            this.btnHesapla.Text = "Maaşı Hesapla";
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // comboBoxEdit1
+            // 
+            this.comboBoxEdit1.Location = new System.Drawing.Point(111, 69);
+            this.comboBoxEdit1.Name = "comboBoxEdit1";
+            this.comboBoxEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBoxEdit1.Properties.Appearance.Options.UseFont = true;
+            this.comboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.comboBoxEdit1.Size = new System.Drawing.Size(338, 34);
+            this.comboBoxEdit1.TabIndex = 21;
+            this.comboBoxEdit1.SelectedIndexChanged += new System.EventHandler(this.comboBoxEdit1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(1494, 593);
+            this.Controls.Add(this.comboBoxEdit1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.labelControl10);
+            this.Controls.Add(this.labelControl9);
+            this.Controls.Add(this.textEdit8);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.labelControl8);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txtGaranti);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.txtBonus);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtTelefon);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtTelefon.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtBonus.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMaas.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGaranti.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit8.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.TextEdit txtTelefon;
+        private DevExpress.XtraEditors.TextEdit txtID;
+        private DevExpress.XtraEditors.TextEdit txtBonus;
+        private DevExpress.XtraEditors.TextEdit txtMaas;
+        private DevExpress.XtraEditors.TextEdit txtGaranti;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.LabelControl labelControl8;
+        private System.Windows.Forms.RichTextBox txtAdres;
+        private DevExpress.XtraEditors.LabelControl labelControl9;
+        private DevExpress.XtraEditors.TextEdit textEdit8;
+        private DevExpress.XtraEditors.LabelControl labelControl10;
+        private DevExpress.XtraEditors.SimpleButton btnHesapla;
+        private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
+    }
+}
+
---
+++ maaşprojesi/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace maaşprojesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server=LAPTOP-KIFERO8A;Integrated security=SSPI;database=MaasTakip";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "SELECT Ad,Soyad FROM tblMaas ORDER BY ID"; // İstediğiniz sıralamaya göre ORDER BY ekleyin
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // İlgili sütunlardan veriyi al
+                    string Ad = reader["Ad"].ToString();
+                    string Soyad = reader["Soyad"].ToString();
+
+                    // ComboBox'a ekleyin
+                    comboBoxEdit1.Properties.Items.Add($"{Ad} {Soyad}");
+                }
+
+                con.Close();
+            }
+        }
+
+        private void comboBoxEdit1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedName = comboBoxEdit1.SelectedItem.ToString(); // ComboBox'tan seçili kişiyi al
+
+            // Seçilen kişinin bilgilerini almak için SQL sorgusunu hazırla
+            string query = "SELECT ID, Telefon, Adres, Garanti, bonus FROM tblMaas WHERE CONCAT(Ad, ' ', Soyad) = @SelectedName";
+
+            using (SqlConnection con = new SqlConnection("Server=LAPTOP-KIFERO8A;Integrated security=SSPI;database=MaasTakip"))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SelectedName", selectedName);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // Veritabanından gelen bilgileri TextBox'lara yaz
+                    txtID.Text = reader["ID"].ToString();
+                    txtTelefon.Text = reader["Telefon"].ToString();
+                    txtAdres.Text = reader["Adres"].ToString();
+                    txtGaranti.Text = reader["Garanti"].ToString();
+                    txtBonus.Text = reader["Bonus"].ToString();
+                }
+
+                con.Close();
+            }
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int garanti = Convert.ToInt32(txtGaranti.Text);
+            int bonus= Convert.ToInt32(txtBonus.Text);
+            double intoran = 0.10; // yüzdelik dilimi bu 0.15 = %15  == 1 de %100 eşittir 
+            double bonussonuc = bonus * intoran;
+            MessageBox.Show(Convert.ToString("Bonusunuz : " + bonussonuc));
+
+
+            double msonuc = bonussonuc + garanti;
+
+            MessageBox.Show(Convert.ToString("Sonuç : " + msonuc));
+            txtMaas.Text = msonuc.ToString();
+
+
+            //   Bonus gelen paranın % 10'u ve garanti paranın toplamı çalışanın son maaşıdır..
+
+
+
+        }
+    }
+    }
+
+
